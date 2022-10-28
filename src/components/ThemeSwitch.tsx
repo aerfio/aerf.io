de251da35a6d4f74ddec1b12aa35b0f864f7b952
Mateusz Puczyński <mati6095@gmail.com>
2022-10-28 21:45:00 +0200

    cleanup theme switcher

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,28 +1,51 @@
 import React from "react";
-import useDarkMode from "@fisch0920/use-dark-mode";
+import { useTheme } from "../utils/utils";
+import { Helmet } from "react-helmet";
 
 const InputID = "theme-switch";
 export const DarkModeToggle = () => {
-  const darkMode = useDarkMode(true, {
-    classNameDark: "dark",
-    classNameLight: "light",
-  });
+  if (typeof window === "undefined") {
+    return null;
+  }
+
+  const { isDarkThemeEnabled, useThemeToggle } = useTheme();
 
   return (
-    <div>
-      <button type="button" onClick={darkMode.disable}>
-        ☀
-      </button>
-      <input
-        type="checkbox"
-        id={InputID}
-        checked={darkMode.value}
-        onClick={darkMode.toggle}
-        readOnly
-      ></input>
-      <button type="button" onClick={darkMode.enable}>
-        ☾
-      </button>
-    </div>
+    <>
+      <section className="mr-5 self-end">
+        <span>☀️</span>
+        <label
+          htmlFor={InputID}
+          className="align-center relative flex h-7 w-12 cursor-pointer justify-between rounded-[100px] bg-primarylight shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] outline-none transition duration-200 dark:bg-primarydark"
+        >
+          <input
+            type="checkbox"
+            id={InputID}
+            defaultChecked={true}
+            onClick={useThemeToggle}
+            // className="invisible h-0 w-0"
+            // readOnly
+          />
+          {/* <button className="round-full absolute top-[2px] left-[2px] h-6 w-6 border-0 border-solid border-primarylight transition duration-300 content-[''] dark:left-[calc(100%-2px)] dark:border-primarydark	" /> */}
+        </label>
+        <span>🌒</span>
+        {/* <button type="button" onClick={darkMode.disable}>
+          ☀
+        </button>
+
+        <button type="button" onClick={darkMode.enable}>
+          ☾
+        </button> */}
+      </section>
+
+      <Helmet
+        meta={[
+          {
+            name: "theme-color",
+            content: true ? "#e37fad" : "#282c35",
+          },
+        ]}
+      />
+    </>
   );
 };
