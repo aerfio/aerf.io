06fd801f8429ff73c75855d56acaf2cb7a7dc67e
Mateusz Puczyński <mati6095@gmail.com>
2022-11-13 18:34:24 +0100

    remove gatsby-plugin-pnpm

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,7 +14,6 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: false,
   plugins: [
-    `gatsby-plugin-pnpm`,
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
