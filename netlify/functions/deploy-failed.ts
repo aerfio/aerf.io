7d2f5d22754e3d11ede51fff33bb16d760e92282
Mateusz Puczyński <mati6095@gmail.com>
2022-12-29 23:13:26 +0100

    fetch ntfy endpoint so that I'm msgd when sth happens

--- netlify/functions/deploy-failed.ts
+++ netlify/functions/deploy-failed.ts
@@ -1,16 +1,25 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext
+    event: HandlerEvent,
+    context: HandlerContext
 ) => {
-console.log(`event body: ${JSON.stringify(event.body)}`);
-  console.log(`context: ${JSON.stringify(context)}`);
+    console.log(`event body: ${JSON.stringify(event.body)}`);
+    console.log(`context: ${JSON.stringify(context)}`);
+    await fetch('https://ntfy.sh', {
+        method: 'POST',
+        body: JSON.stringify({
+            "topic": process.env.TOPIC_NAME,
+            "title": "Deploy to netlify failed",
+            "tags": ["facepalm"],
+            "priority": 3,
+        })
+    })
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" }),
+    };
 };
 
 export { handler };
--- netlify/functions/deploy-succeeded.ts
+++ netlify/functions/deploy-succeeded.ts
@@ -7,6 +7,17 @@ const handler: Handler = async (
     console.log(`event body: ${JSON.stringify(event.body)}`);
     console.log(`context: ${JSON.stringify(context)}`);
 
+    await fetch('https://ntfy.sh', {
+        method: 'POST',
+        body: JSON.stringify({
+            "topic": process.env.TOPIC_NAME,
+            "title": "Deploy to netlify succeeded",
+            "tags": ["partying_face"],
+            "priority": 2,
+            "click": "https://aerf.io",
+        })
+    })
+
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Hello World" }),
