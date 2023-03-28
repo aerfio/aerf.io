6dac444b382de018264b37cdd9b0ab8b5ac5679b
Mateusz Puczyński <mati6095@gmail.com>
2023-03-28 23:04:32 +0200

    remove trailing slash

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,6 +13,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: false,
+  trailingSlash: "never",
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
